be905f8f0eeb67e8835c19fc9b9de79750eca2c3
Xopppek <xopppekest@gmail.com>
2024-09-10 00:29:04 +0300

    implemented bisection method

---
+++ lab1/bisection_method.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def bisection_method(f, a, b, epsilon = 0.00001):
+    '''
+    На вход подается функция func,
+    a, b - границы поиска корня
+
+    Возвращает решение уравнения f(x) = 0 на отрезке [a, b]
+    '''
+    x = (a + b)/2
+    while abs(b-a) > 2*epsilon:
+        if f(x) * f(a) < 0:
+            b = x
+        else:
+            a = x
+        x = (a + b)/2
+    return x
